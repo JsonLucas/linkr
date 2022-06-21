b0c2ea6e060c892f52291f6cf07673dacf513cd0
Eduardo <eduyudji@gmail.com>
2022-06-21 14:37:19 -0300

    login Page mobile

--- src/components/LoginForm/style.js
+++ src/components/LoginForm/style.js
@@ -5,6 +5,14 @@ export const Container = styled.div`
     height: 100%;
     width: 535px;
     background-color: #333333;
+    @media (max-width: 425px) {
+        width: 100%;
+        height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 auto;
+    }
 `;
 
 export const RowForm = styled.form`
@@ -22,9 +30,18 @@ export const RowField = styled.div`
 export const Field = styled.input`
     box-sizing: border-box;
     width: 100%;
-    padding: 15px 10px 15px 10px;
+    height: 55px;
+    padding-left: 17px;
     border: none;
     border-radius: 5px;
+    @media (max-width:425px) {
+            ::placeholder{
+            font-family: 'Oswald', sans-serif; 
+            font-weight: 700;
+            font-size: 22px;
+            color: #9F9F9F;
+        }
+    }
 `;
 
 export const SubmitButton = styled.button`
@@ -42,6 +59,9 @@ export const SubmitButton = styled.button`
         width: 20%;
         margin: auto;
     }
+    @media (max-width:425px) {
+        font-size: 22px;
+    }
 `; 
 
 export const RowSwitchPage = styled.p`
@@ -51,4 +71,13 @@ export const RowSwitchPage = styled.p`
     color: white;
     font-size: 20px;
     text-decoration: underline;
+    @media (max-width:425px) {
+        font-family: 'Lato', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17px;
+        line-height: 20px;
+        text-decoration-line: underline;
+        color: #FFFFFF;
+    }
 `;
